b37ac94e60358cffc73868d956c94c890bb43dce
BoGGoG <marco.knipfer@me.com>
2022-08-06 18:04:46 +0200

    Probably finished simplify script, need to test and run

--- 2022-07-29-DataPreparation/scripts/DataPreparation.py
+++ 2022-07-29-DataPreparation/scripts/DataPreparation.py
@@ -10,6 +10,9 @@ import sympy as sp
 # from tqdm.notebook import tqdm
 from tqdm import tqdm
 from datetime import datetime
+import multiprocessing as mp
+from parallelbar import progress_imap, progress_map
+
   
 # current = os.path.dirname(os.path.realpath(__file__))
 # parent = os.path.dirname(current)
@@ -32,6 +35,7 @@ amplitudes_folders_names = ["1to2/", "2to1/", "2to2/",]# "2to3/", "3to2/", "3to3
 amplitudes_folders = [ampl_folders_prefix+a for a in amplitudes_folders_names]
 sqamplitudes_raw_folders_names = ["1to2/", "2to1/", "2to2/",]# "2to3/", "3to2/", "3to3/",]
 sqamplitudes_folders = [sqampl_raw_folders_prefix+a for a in sqamplitudes_raw_folders_names]
+cpus = 10
 #
 # # %%
 # # # should really parallelize the simplify etc. Here is a link:
@@ -128,34 +132,91 @@ all_sqamplitudes_unique = [all_sqamplitudes[i] for i in unique_indices]
 # I will still keep all of them.
 
 progress_file = "log/progress.log"
-overwrite = True   # overwrite progress_file
+outfile_amplitudes =  "../data.nosync/QED_amplitudes_TreeLevel_UpTo3to3.txt"
+outfile_sqamplitudes =  "../data.nosync/QED_sqamplitudes_TreeLevel_UpTo3to3_simplified.txt"
+start_fresh = True   # overwrite progress_file
 batch_size = 20
 batches = len(unique_indices) // batch_size + 1
-ic(batches)
+batch_resume = 0
 
-if overwrite:
+if start_fresh:
+    print("Starting fresh. Deleting "+progress_file)
     if os.path.exists(progress_file):
         os.remove(progress_file)
+    if os.path.exists(outfile_amplitudes):
+        os.remove(outfile_amplitudes)
+    if os.path.exists(outfile_sqamplitudes):
+        os.remove(outfile_sqamplitudes)
 
-for batch in range(batches):
+    with open(progress_file, 'a') as f:
+        f.write("Starting calculation at"+str(datetime.now())+"\n")
+        f.write("batch_size:"+str(batch_size)+"\n")
+        f.write("lines:"+str(len(unique_indices))+"\n")
+        f.write("batches:"+str(batches)+"\n")
+        f.write("----------------------\n")
+        f.write("----------------------\n")
+
+else:
+    print("Resuming calculations, reading progress from "+progress_file)
+    with open(progress_file) as f:
+        progess_file_contents = [line for line in f.readlines()]
+    # print(progess_file_contents[-6:])
+    batch_resume = int(progess_file_contents[-7].split(":")[1]) + 1
+    index_resume = int(progess_file_contents[-3].split(":")[1])
+    batch_size_resume = int(progess_file_contents[-2].split(":")[1])
+    assert batch_resume*batch_size_resume == index_resume
+    batch_size = batch_size_resume
+    ic(batch_resume)
+    ic(index_resume)
+    ic(batch_size_resume)
+    print("Continuing with batch " +str(batch_resume)+"/"+str(batches)+", which amounts to line "+str(index_resume)+".")
+    if batch_resume == batches:
+        print("Nothing to resume, already finished.")
+
+batches = range(batch_resume, batches)
+
+for batch in batches[0:2]:
     batch_start_index = batch*batch_size
     batch_end_index = (batch+1)*batch_size
+    sqamplitudes_batch = all_sqamplitudes_unique[batch_start_index:batch_end_index]
+    amplitudes_batch = all_amplitudes_unique[batch_start_index:batch_end_index]
 
     start_time = datetime.now()
-    # do simplificaion etc
-    # write out to file etc
+    # ------------------------------
+    with mp.Pool(processes=cpus) as p:
+        # sqamplitudes_simplified_prefix_batch = progress_map(simplify_and_prefix, sqamplitudes_batch, n_cpu=cpus)  #, core_progress=True)
+        sqamplitudes_simplified_prefix_batch = p.map(simplify_and_prefix, sqamplitudes_batch)
+    ic(len(sqamplitudes_batch))
+    ic(len(sqamplitudes_simplified_prefix_batch))
+    ## output not working yet!
+    ## try to append to file in batches and not each line
+    out_amplitudes_str = [",".join(x) for x in amplitudes_batch]
+    out_amplitudes_str = "\n".join(out_amplitudes_str)
+    out_sqamplitudes_str = [",".join(x) for x in sqamplitudes_simplified_prefix_batch]
+    out_sqamplitudes_str = "\n".join(out_sqamplitudes_str)
+    print(out_amplitudes_str)
+
+    with open(outfile_amplitudes, "a") as f:
+        f.write(out_amplitudes_str)
+    with open(outfile_sqamplitudes, "a") as f:
+        f.write(out_sqamplitudes_str)
+
+    # ------------------------------
     end_time = datetime.now()
 
     with open(progress_file, 'a') as f:
+        f.write("Appended amplitudes to "+outfile_amplitudes)
+        f.write("Appended sqamplitudes to "+outfile_sqamplitudes)
         f.write("batch:" + str(batch) + "\n")
         f.write("started at:" + str(start_time) + "\n")
         f.write("finished at:" + str(end_time) + "\n")
         f.write("batch_start_index:" + str(batch_start_index) + "\n")
         f.write("batch_end_index:" + str(batch_end_index) + "\n")
+        f.write("batch_size:" + str(batch_size) + "\n")
         f.write("----------------------\n")
 
+
 # # %%
-# import multiprocessing as mp
 # # pool = mp.Pool(mp.cpu_count())
 # ic(mp.cpu_count())
 # cpus = 16
@@ -173,10 +234,9 @@ for batch in range(batches):
 # #     return result_list_tqdm
 #
 # # %%
-# from parallelbar import progress_imap, progress_map
 #
 # # %%
-# from lib2to3.pgen2.literals import simple_escapes
+# # from lib2to3.pgen2.literals import simple_escapes
 #
 #
 # with mp.Pool(processes=cpus) as p:
