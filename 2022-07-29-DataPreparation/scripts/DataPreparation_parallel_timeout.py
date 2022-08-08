3bb5f907af2c3c3e43e154af1f4464fc8115f1d4
BoGGoG <marco.knipfer@me.com>
2022-08-08 09:45:34 +0200

    Parallelization with timtout

--- 2022-07-29-DataPreparation/scripts/DataPreparation_parallel_timeout.py
+++ 2022-07-29-DataPreparation/scripts/DataPreparation_parallel_timeout.py
@@ -11,8 +11,10 @@ import sympy as sp
 from tqdm import tqdm
 from datetime import datetime
 import multiprocessing as mp
-from parallelbar import progress_imap, progress_map
-
+import multiprocessing.queues as mpq
+import functools
+import dill
+from typing import Tuple, Callable, Dict, Optional, Iterable, List
   
 # current = os.path.dirname(os.path.realpath(__file__))
 # parent = os.path.dirname(current)
@@ -25,44 +27,153 @@ from source.read_amplitudes import read_amplitudes, read_amplitudes_and_raw_squa
 import sympy as sp
 from source.SympyPrefix import prefix_to_sympy, sympy_to_prefix, simplify_and_prefix
 
-  
+# -------------------------------------------------------------------------------------------  
+
+class TimeoutError(Exception):
+
+    def __init__(self, func, timeout):
+        self.t = timeout
+        self.fname = func.__name__
+
+    def __str__(self):
+            return f"function '{self.fname}' timed out after {self.t}s"
+
+
+def _lemmiwinks(func: Callable, args: Tuple[object], kwargs: Dict[str, object], q: mp.Queue):
+    """lemmiwinks crawls into the unknown"""
+    q.put(dill.loads(func)(*args, **kwargs))
+
+
+def killer_call(func: Callable = None, timeout: int = 10) -> Callable:
+    """
+    Single function call with a timeout
+
+    Args:
+        func: the function
+        timeout: The timeout in seconds
+    """
+
+    if not isinstance(timeout, int):
+        raise ValueError(f'timeout needs to be an int. Got: {timeout}')
+
+    if func is None:
+        return functools.partial(killer_call, timeout=timeout)
+
+    @functools.wraps(killer_call)
+    def _inners(*args, **kwargs) -> object:
+        q_worker = mp.Queue()
+        proc = mp.Process(target=_lemmiwinks, args=(dill.dumps(func), args, kwargs, q_worker))
+        proc.start()
+        try:
+            return q_worker.get(timeout=timeout)
+        except mpq.Empty:
+            raise TimeoutError(func, timeout)
+        finally:
+            try:
+                proc.terminate()
+            except:
+                pass
+    return _inners
+
+
+def _queue_mgr(func_str: str, q_in: mp.Queue, q_out: mp.Queue, timeout: int, pid: int, timeout_logfile: str) -> object:
+    """
+    Controls the main workflow of cancelling the function calls that take too long
+    in the parallel map
+
+    Args:
+        func_str: The function, converted into a string via dill (more stable than pickle)
+        q_in: The input queue
+        q_out: The output queue
+        timeout: The timeout in seconds
+        pid: process id
+    """
+    while not q_in.empty():
+        positioning, x  = q_in.get()
+        q_worker = mp.Queue()
+        proc = mp.Process(target=_lemmiwinks, args=(func_str, (x,), {}, q_worker,))
+        proc.start()
+        try:
+            # print(f'[{pid}]: {positioning}: getting')
+            res = q_worker.get(timeout=timeout)
+            # print(f'[{pid}]: {positioning}: got')
+            q_out.put((positioning, res))
+        except mpq.Empty:
+            q_out.put((positioning, x))
+            print(f'[{pid}]: {positioning}: timed out ({timeout}s)')
+            with open(timeout_logfile, "a") as f:
+                f.write("Timed out after "+str(timeout)+" seconds. Argument:" + x + "\n")
+        finally:
+            try:
+                proc.terminate()
+                # print(f'[{pid}]: {positioning}: terminated')
+            except:
+                pass
+    # print(f'[{pid}]: completed!')
+
+
+def killer_pmap(func: Callable, iterable: Iterable, cpus: Optional[int] = None, timeout: int = 10,
+        timeout_logfile = "log/timeout_log.log"):
+    """
+    Parallelisation of func across the iterable with a timeout at each evaluation
+
+    Args:
+        func: The function
+        iterable: The iterable to map func over
+        cpus: The number of cpus to use. Default is the use max - 2.
+        timeout: kills the func calls if they take longer than this in seconds
+    """
+
+    if cpus is None:
+        cpus = max(mp.cpu_count() - 2, 1)
+        if cpus == 1:
+            raise ValueError('Not enough CPUs to parallelise. You only have 1 CPU!')
+        else:
+            print(f'Optimising for {cpus} processors')
+
+    q_in = mp.Queue()
+    q_out = mp.Queue()
+    sent = [q_in.put((i, x)) for i, x in enumerate(iterable)]
+
+    processes = [
+        mp.Process(target=_queue_mgr, args=(dill.dumps(func), q_in, q_out, timeout, pid, timeout_logfile))
+        for pid in range(cpus)
+    ]
+    print(f'Started {len(processes)} processes')
+    for proc in processes:
+        proc.start()
+
+    result = [q_out.get() for _ in sent]
+
+    for proc in processes:
+        proc.terminate()
+
+    return [x for _, x, in sorted(result)]
+
+
+# -------------------------------------------------------------------------------------------  
 
 # # %%
 ampl_folders_prefix = "../QED_AllParticles_IO/out/ampl/"
 # sqampl_folders_prefix = "../QED_AllParticles_IO/out/sq_ampl/"
 sqampl_raw_folders_prefix = "../QED_AllParticles_IO/out/sq_ampl_raw/"
-amplitudes_folders_names = ["1to2/", "2to1/", "2to2/",]# "2to3/", "3to2/", "3to3/",]
+amplitudes_folders_names = ["1to2/", "2to1/", "2to2/", "2to3/", "3to2/", "3to3/",]
+# amplitudes_folders_names = ["1to2/", "2to1/", "2to2/", "2to3/", ]# "3to2/", "3to3/",]
 amplitudes_folders = [ampl_folders_prefix+a for a in amplitudes_folders_names]
-sqamplitudes_raw_folders_names = ["1to2/", "2to1/", "2to2/",]# "2to3/", "3to2/", "3to3/",]
+sqamplitudes_raw_folders_names = ["1to2/", "2to1/", "2to2/", "2to3/", "3to2/", "3to3/",]
+# sqamplitudes_raw_folders_names = ["1to2/", "2to1/", "2to2/", "2to3/", ]# "3to2/", "3to3/",]
 sqamplitudes_folders = [sqampl_raw_folders_prefix+a for a in sqamplitudes_raw_folders_names]
 cpus = 19
+timeout_s = 60*30   # timeout in seconds
 
 progress_file = "log/progress_up_to_3to3.log"
 outfile_amplitudes =  "../data.nosync/QED_amplitudes_TreeLevel_UpTo3to3.txt"
 outfile_sqamplitudes =  "../data.nosync/QED_sqamplitudes_TreeLevel_UpTo3to3_simplified.txt"
+timeout_logfile = "log/timeout_log.log"
 start_fresh = True   # overwrite progress_file
-batch_size = 20
+batch_size = 5000
 batch_resume = 0
 
-#
-# # %%
-# # # should really parallelize the simplify etc. Here is a link:
-# # # https://www.delftstack.com/howto/python/parallel-for-loops-python/
-# # # the code will look like this:
-#
-# # import multiprocessing
-#
-#
-# # def sumall(value):
-# #     return sum(range(1, value + 1))
-#
-# # pool_obj = multiprocessing.Pool()
-#
-# # answer = pool_obj.map(sumall,range(0,5))
-# # print(answer)
-#
-# # %%
-#
 amplitudes = dict()
 sqamplitudes = dict()
 for amplitudes_folder, sqamplitudes_folder, name in zip(amplitudes_folders, sqamplitudes_folders, amplitudes_folders_names):
@@ -150,6 +261,9 @@ if start_fresh:
     if os.path.exists(outfile_sqamplitudes):
         print("Deleting "+outfile_sqamplitudes)
         os.remove(outfile_sqamplitudes)
+    if os.path.exists(timeout_logfile):
+        print("Deleting "+timeout_logfile)
+        os.remove(timeout_logfile)
 
     with open(progress_file, 'a') as f:
         f.write("Starting calculation at:"+str(datetime.now())+"\n")
@@ -189,11 +303,15 @@ for batch in tqdm(batches):
 
     start_time = datetime.now()
     # ------------------------------
-    with mp.Pool(processes=cpus) as p:
+    sqamplitudes_simplified_prefix_batch = killer_pmap(simplify_and_prefix, sqamplitudes_batch, cpus=cpus, timeout=timeout_s)
+
+    # with mp.Pool(processes=cpus) as p:
         # sqamplitudes_simplified_prefix_batch = progress_map(simplify_and_prefix, sqamplitudes_batch, n_cpu=cpus)  #, core_progress=True)
-        sqamplitudes_simplified_prefix_batch = p.map(simplify_and_prefix, sqamplitudes_batch)
+        # sqamplitudes_simplified_prefix_batch = p.map(simplify_and_prefix, sqamplitudes_batch)
     ## output not working yet!
     ## try to append to file in batches and not each line
+
+    # ------------------------------
     out_amplitudes_str = [",".join(x) for x in amplitudes_batch]
     out_amplitudes_str = "\n".join(out_amplitudes_str)+"\n"
     out_sqamplitudes_str = [",".join(x) for x in sqamplitudes_simplified_prefix_batch]
